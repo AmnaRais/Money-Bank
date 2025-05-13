97201c35ce663e488d4987fec76fec00c8ebdb6e
aimenkhan1 <aimannadeem701@gmail.com>
2025-05-13 19:12:01 +0000

    Add files via upload

---
+++ MenuDriven.h
@@ -0,0 +1,24 @@
+#ifndef MENUDRIVEN_H
+#define MENUDRIVEN_H
+
+#pragma once
+
+#include <iostream>
+#include <regex>
+#include <conio.h>
+#include <cstring>
+
+using namespace std;
+
+class MenuDriven{
+private:
+
+public:
+    MenuDriven();
+	void Intro();
+    static bool Email_check(string);
+    static string mask_password();
+    static bool Check_StudentID(string);
+};
+
+#endif
